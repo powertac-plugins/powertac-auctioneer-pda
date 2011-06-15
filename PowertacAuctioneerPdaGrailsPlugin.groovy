80c437c29eb596b45dd00e552196335a21fbf7e5
John Collins <jcollins@cs.umn.edu>
2011-06-14 20:34:29 -0500

    prep for release 0.3

--- PowertacAuctioneerPdaGrailsPlugin.groovy
+++ PowertacAuctioneerPdaGrailsPlugin.groovy
@@ -1,12 +1,12 @@
 class PowertacAuctioneerPdaGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.4 > *"
+    def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['powertacCommon':'0.9 > *',
-                     'powertacServerInterface':'0.1 > *',
-                     'powertacAccountingService': '0.2 > *']
+    def dependsOn = ['powertacCommon':'0.10 > *',
+                     'powertacServerInterface':'0.2 > *',
+                     'powertacAccountingService': '0.4 > *']
     
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
